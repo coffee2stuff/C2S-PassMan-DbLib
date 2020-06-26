f8dcb9b2b661793e894e6f1da36d3ed694a3e4ca
Peter Aleksander Bizjak <coffee2stuff.converter@gmail.com>
2020-06-26 15:05:50 +0000

    Remove the test implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
     implementation(group = "org.mongodb", name = "mongodb-driver-sync", version = "4.1.0-beta2")
     implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-reactive", version = "1.3.7")
     implementation(group = "org.slf4j", name = "slf4j-log4j12", version = "1.7.30")
-    testImplementation("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
