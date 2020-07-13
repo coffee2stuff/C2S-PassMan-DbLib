78b2659f1dfed57b33b6469bdebf2f529c5afece
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-07-13 10:42:16 +0200

    Bump version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,6 @@
 import di.mongoModule
-import models.LoginModel
-import models.NoteModel
-import models.UserModel
 import org.apache.log4j.BasicConfigurator
 import org.koin.core.context.startKoin
-import utils.helpers.generateNumericUUID
-
-private val service = MongoService()
-
-private const val accessToken = "e2bd12e6-c361-11ea-b4df-54ee75f47269"
 
 fun main() {
     BasicConfigurator.configure()
@@ -16,64 +8,4 @@ fun main() {
     startKoin {
         modules(mongoModule)
     }
-
-    // TODO: remove after development!
-    userOperations()
-    loginOperations()
-    noteOperations()
-}
-
-private fun userOperations() {
-    val uuid = generateNumericUUID()
-    val createUserResult = service.createUser(
-        UserModel(
-            uuid,
-            accessToken,
-            "John Doe",
-            "john.doe@gmail.com",
-            "myNameIsJohn"
-        )
-    )
-    println("Create user: $createUserResult")
-
-    val readUserResultById = service.fetchUser(true, uuid)
-    val readUserByEmailPassword = service.fetchUser(false, "john.doe@gmail.com", "myNameIsJohn")
-    println("The user fetches by ID ($readUserResultById) and by email/password ($readUserByEmailPassword) should match")
-
-    val updateUserResult = service.updateUser(UserModel(uuid, accessToken, "John Smith", "john.smith-doe@gmail.com", "myNameIsJohnButMySurnameDifferent"))
-    println("Update user: $updateUserResult")
-}
-
-private fun loginOperations() {
-    val uuid = generateNumericUUID()
-
-    val createLoginResult = service.createLogin(
-        LoginModel(
-            uuid,
-            accessToken,
-            "Gmail",
-            "john.doe@gmail.com",
-            "testPasswordYolo",
-            "accounts.google.com"
-        )
-    )
-    println("Create login: $createLoginResult")
-
-    val readLoginResult = service.fetchLogin(uuid, accessToken)
-    println("Read login: $readLoginResult")
-
-    val readAllLoginsResult = service.fetchAllLogins(accessToken)
-    println("Number of all logins in the database: ${readAllLoginsResult.size}")
-}
-
-
-private fun noteOperations() {
-    for (i in 0..3) {
-        val createNoteResult = service.createNote(NoteModel(generateNumericUUID(), accessToken, "Note #$i is here!"))
-        println("Create note: $createNoteResult")
-    }
-
-    val readAllNotesResult = service.fetchAllNotes(accessToken)
-    println("Number of all notes in the database: ${readAllNotesResult.size}")
-
 }
